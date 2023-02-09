6c59ea930e38ff28a71c2b388e377e4bb4d1bee7
randycane <randychang97@gmail.com>
2023-02-09 02:44:24 -0800

    remove duplicate logic and add pointer

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -12,7 +12,7 @@ import LoginModalComponent from "../LoginModal/Login";
 const NavBarComponent = ({ isLoaded }) => {
   const history = useHistory();
   const sessionUser = useSelector((state) => state?.session?.user);
-  const [loginFormModalIsOpen, setLoginFormModalIsOpen] = useState(false);
+  //const [loginFormModalIsOpen, setLoginFormModalIsOpen] = useState(false);
 
   const signUpPage = (subrandditId) => {
     let path = `/sign-up`;
@@ -27,27 +27,8 @@ const NavBarComponent = ({ isLoaded }) => {
         </div>
       </>
     );
-  } else {
-    sessionLinks = (
-      <div id="rightNav">
-        <button className="signUpButton" onClick={signUpPage}>
-          {" "}
-          Sign up
-        </button>
-        <SignUpForm />
-        <button
-          className="signInButton"
-          onClick={() => setLoginFormModalIsOpen(true)}
-        >
-          Log In
-        </button>
-        <LoginModalComponent
-          isOpen={loginFormModalIsOpen}
-          modalToggle={setLoginFormModalIsOpen}
-        />
-      </div>
-    );
   }
+
 
   return (
     <div className="whole-navi">
@@ -70,46 +51,7 @@ const NavBarComponent = ({ isLoaded }) => {
     </div>
   );
 
-  // function NavBarComponent() {
 
-  //   const userIsLoggedIn = useSelector((state) => state?.session?.user)
-
-  //   let sessionLinks;
-
-  //   if (userIsLoggedIn) {
-  //     sessionLinks = (
-  //       <ProfileButtonComponent user={userIsLoggedIn}/>
-  //     )
-  //   } else {
-  //     sessionLinks = (
-  //       <>
-  //         <LoginFormModal />
-  //         <SignupFormModal />
-  //         </>
-  //     )
-  //   }
-  //   return (
-  //     <div className="top">
-  //         <div className="home">
-  //           <NavLink to='/' exact={true} activeClassName='active'>
-  //             <div className="home-icon">
-  //             <img className="img-home" src="https://res.cloudinary.com/teepublic/image/private/s--rdSMZL_M--/c_fit,g_north_west,h_711,w_840/co_484849,e_outline:40/co_484849,e_outline:inner_fill:1/co_ffffff,e_outline:40/co_ffffff,e_outline:inner_fill:1/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1592405498/production/designs/11418386_0.jpg" alt="nope"></img>
-  //             <div className="home-text">
-  //               Randdit
-  //               </div>
-  //             </div>
-  //         </NavLink>
-  //             </div>
-  //             <div className="page-title">
-  //                 Where all weebs can live in harmony.
-  //       </div>
-
-  //       <div className="get-on-sib">
-  //         {sessionLinks}
-  //         </div>
-
-  //     </div>
-  //   );
 };
 
 export default NavBarComponent;
