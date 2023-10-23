6e3ec4220886c63f354af44f8bf3ff7e4a6e1570
randycane <randychang97@gmail.com>
2023-10-23 14:45:05 -0700

    validations

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -6,7 +6,7 @@ from wtforms.fields import (
 )
 
 class PostForm(FlaskForm):
-  post_title = StringField("post_title", validators=[DataRequired("Title is required!"), Length(min=3, max=200, message="Post title should be between 3 and 120 characters long")])
+  post_title = StringField("post_title", validators=[DataRequired("Title is required!"), Length(min=3, max=200, message="Post title should be between 3 and 200 characters long")])
   img_url = StringField("img_url")
   post_text = TextAreaField("post_text", validators=[Length(max=2000, message="Text length must be less than 2000 characters!")], filters = [lambda x: x or None])
   subranddit_id = IntegerField("subranddit_id")
